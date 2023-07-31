1b1db3849d0b04c7275a7f1814000d1880a30f37
belemaaa <graceitamunoala@gmail.com>
2023-07-31 11:30:14 +0000

    type animation

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import resumeSample from '../assets/images/resume-sample.jpg'
+import { Link } from 'react-router-dom'
+import { TypeAnimation } from 'react-type-animation'
 
 const Landing = () => {
   return (
-    <div>Landing</div>
+    <div className='landing'>
+        <div className='hidden md:flex resume-sample'>
+            <img src={resumeSample}/>
+        </div>
+
+        <div className='landing-texts'>
+            <p>Build Your  
+                <span className='resume-text'>
+                <TypeAnimation 
+                    sequence={['RESUME', 1000, 'Curriculum Vitae', 1000]}
+                    speed={10}
+                    repeat={Infinity}
+                />
+                </span>
+            </p>
+            <p>In Minutes</p>
+
+            <p>Get a personalized resume designed for you with any information
+                you want. 
+            </p>
+        </div>
+
+        <div>
+            <Link to='#'>
+                Get Started
+            </Link>
+        </div>
+    </div>
   )
 }
 
