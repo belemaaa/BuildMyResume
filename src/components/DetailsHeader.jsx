a3ab88b8081567950d38cd6cbe4776c87c073328
belemaaa <graceitamunoala@gmail.com>
2023-07-31 12:57:47 +0000

    form update

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -11,53 +11,57 @@ const DetailsHeader = () => {
         setDetails1Array([...details1Array, formData]);
     };
   return (
-    <div>
-        <div className='details-div'>
-            <p>To build your resume, please fill out the fields below</p>
-            <p>Note: These details will represent the fields in your resume.</p>
+    <div className='details-page'>
+        <div className='details-div shadow-lg'>
+            <p className='p1'>To build your resume, please fill out the fields below</p>
+            <p className='p2'>Note: These details will represent the fields in your resume.</p>
+
+            <form method='POST' onSubmit={handleFormSubmit} className='details1form'>
+                <div className='details-box'>
+                    <label className='details-lbl'>Fullname:</label> <br/>
+                    <input 
+                        className='border'
+                        type='text'
+                        maxLength={100}
+                        onChange={(e) => setFullname(e.target.value)}
+                        required
+                    />
+                </div>
+
+                <div className='details-box'>
+                    <label>Address:</label><br/>
+                    <input 
+                        type='text'
+                        className='border'
+                        maxLength={250}
+                        onChange={(e) => setAddress(e.target.value)}
+                        required
+                    />
+                </div>
+
+                <div className='details-box'>
+                    <label>Phone:</label><br/>
+                    <input 
+                        type='phone'
+                        className='border'
+                        onChange={(e) => setPhone(e.target.value)}
+                        required
+                    />
+                </div>                    
+
+                <div className='details-box'>
+                    <label>Email:</label><br/>
+                    <input 
+                        type='email'
+                        className='border'
+                        maxLength={250}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+
+            </form>
         </div>
-
-        <form method='POST' onSubmit={handleFormSubmit}>
-            <label>Fullname:</label> <br/>
-            <input 
-                className='details-input-box border'
-                type='text'
-                maxLength={100}
-                onChange={(e) => setFullname(e.target.value)}
-                required
-            />
-            <br/>
-
-            <label>Address:</label><br/>
-            <input 
-                type='text'
-                className='details-input-box border'
-                maxLength={250}
-                onChange={(e) => setAddress(e.target.value)}
-                required
-            />
-            <br/>
-
-            <label>Phone:</label><br/>
-            <input 
-                type='phone'
-                className='details-input-box border'
-                onChange={(e) => setPhone(e.target.value)}
-                required
-            />
-            <br/>
-
-            <label>Email:</label><br/>
-            <input 
-                type='email'
-                className='details-input-box border'
-                maxLength={250}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-            />
-
-        </form>
-
     </div>
   )
 }
