caaa9f155f13a54efd0fdcd119fcd1586f6fe247
belemaaa <graceitamunoala@gmail.com>
2023-07-31 12:38:25 +0000

    details1 form

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -5,6 +5,11 @@ const DetailsHeader = () => {
     const [address, setAddress] = useState('')
     const [phone, setPhone] = useState('')
     const [email, setEmail] = useState('')
+
+    const [details1Array, setDetails1Array] = useState([]);
+    const handleFormSubmit = (formData) => {
+        setDetails1Array([...details1Array, formData]);
+    };
   return (
     <div>
         <div className='details-div'>
@@ -12,16 +17,47 @@ const DetailsHeader = () => {
             <p>Note: These details will represent the fields in your resume.</p>
         </div>
 
-        <form method='POST'>
-            <label>Fullname</label> <br/>
+        <form method='POST' onSubmit={handleFormSubmit}>
+            <label>Fullname:</label> <br/>
             <input 
-                className='details-input-box'
+                className='details-input-box border'
                 type='text'
                 maxLength={100}
+                onChange={(e) => setFullname(e.target.value)}
+                required
+            />
+            <br/>
+
+            <label>Address:</label><br/>
+            <input 
+                type='text'
+                className='details-input-box border'
+                maxLength={250}
+                onChange={(e) => setAddress(e.target.value)}
+                required
+            />
+            <br/>
+
+            <label>Phone:</label><br/>
+            <input 
+                type='phone'
+                className='details-input-box border'
+                onChange={(e) => setPhone(e.target.value)}
+                required
+            />
+            <br/>
+
+            <label>Email:</label><br/>
+            <input 
+                type='email'
+                className='details-input-box border'
+                maxLength={250}
+                onChange={(e) => setEmail(e.target.value)}
                 required
             />
 
         </form>
+
     </div>
   )
 }
