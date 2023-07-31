ff8ec9abf0edc12014e755e2aa19e13a441e0947
belemaaa <graceitamunoala@gmail.com>
2023-07-31 10:48:18 +0000

    splash

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SplashScreen from './components/SplashScreen';
 
 function App() {
   return (
     <div>
-      
+      <Router>
+        <Routes>
+          <Route path='/' exact element={<SplashScreen/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/SplashScreen.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {GrResume} from 'react-icons/gr'
+import '../assets/css/styles.css'
+
+const SplashScreen = () => {
+  return (
+    <div className='splash'>
+        <div className='splash-text'>
+            <GrResume/>
+            <p>BuildMyResume</p>
+        </div>
+    </div>
+  )
+}
+
+export default SplashScreen
