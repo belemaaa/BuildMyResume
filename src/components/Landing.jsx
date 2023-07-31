c478bb460aa4ca2a9470eec9ef8fd232b0f4f5e2
belemaaa <graceitamunoala@gmail.com>
2023-07-31 11:57:31 +0000

    landing

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -6,31 +6,38 @@ import { TypeAnimation } from 'react-type-animation'
 const Landing = () => {
   return (
     <div className='landing'>
-        <div className='hidden md:flex resume-sample'>
+        <div className='hidden md:flex resume-sample drop-shadow-lg'>
             <img src={resumeSample}/>
         </div>
 
-        <div className='landing-texts'>
-            <p>Build Your  
-                <span className='resume-text'>
-                <TypeAnimation 
-                    sequence={['RESUME', 1000, 'Curriculum Vitae', 1000]}
-                    speed={10}
-                    repeat={Infinity}
-                />
-                </span>
-            </p>
-            <p>In Minutes</p>
+        <div className='flex flex-col'>
+            <div>
+                <div className='landing-text-L'>
+                    <p className=''>Build Your  
+                        <span className='resume-text'>
+                            <TypeAnimation 
+                                sequence={['RESUME', 1000, 'Curriculum Vitae', 1000]}
+                                speed={10}
+                                repeat={Infinity}
+                            />
+                        </span>
+                    </p>
+                    <p>In Minutes</p>
+                </div>
 
-            <p>Get a personalized resume designed for you with any information
-                you want. 
-            </p>
-        </div>
+                <div className='landing-text-S'>
+                    <p>
+                        Get a personalized resume designed for you with any information
+                        you want. 
+                    </p>
+                </div>
+            </div>
 
-        <div>
-            <Link to='#'>
-                Get Started
-            </Link>
+            <div className='get-started-btn'>
+                <Link to='#'>
+                    Get Started
+                </Link>
+            </div>
         </div>
     </div>
   )
