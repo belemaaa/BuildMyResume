e12b8ad4eb9da88e0123255cb8effe35fb6dfc14
belemaaa <graceitamunoala@gmail.com>
2023-08-01 10:51:58 +0000

    loader

--- src/components/GeneralInformation.jsx
+++ src/components/GeneralInformation.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import { useNavigate } from 'react-router-dom'
+import SquareLoader from "react-spinners/SquareLoader";
 
 const GeneralInformation = () => {
     const [fullname, setFullname] = useState('')
@@ -9,91 +10,117 @@ const GeneralInformation = () => {
     const [details, setDetails] = useState('')
     const navigate = useNavigate()
 
-    const [details1Array, setDetails1Array] = useState([]);
-      const handleFormSubmit = (e) => {
-        e.preventDefault();
-        const formData = {
-          fullname,
-          address,
-          phone,
-          email,
-          details,
-        };
-        setDetails1Array([...details1Array, formData]);
+    const [details1Array, setDetails1Array] = useState({});
 
-         // Save the form data to localStorage after the form submission
-        localStorage.setItem('generalInformation', JSON.stringify([...details1Array, formData]));
-    
-        navigate('/resume');
+    const handleFormSubmit = (e) => {
+      e.preventDefault();
+      const formData = {
+        fullname,
+        address,
+        phone,
+        email,
+        details,
+      };
+      setDetails1Array(formData);
+  
+      // Save the form data to localStorage as an object
+      localStorage.setItem('generalInformation', JSON.stringify(formData));
+  
+      navigate('/personal_data');
     };
+
+    // loader
+    const [loading, setLoading] = useState(false)
+    useEffect(() => {
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, 5000)
+    }, [])
 
   return (
     <div className='details-page'>
-        <div className='details-div'>
-            <p className='p1'>To build your resume, please fill out the fields below</p>
-            <p className='p2'>Note: These details will represent the fields in your resume.</p>
+        {
+            loading ? 
+                <SquareLoader
+                    color={'#000000'}
+                    loading={loading}
+                    size={60}
+                    aria-label="Loading Spinner"
+                    data-testid="loader"
+                    className='loader'
+                />
+            :
 
-            <p className='form-heading'>(GENERAL INFORMATION)</p>
+                <div className='details-div'>
+                    <p className='p1'>To build your resume, please fill out the fields below</p>
+                    <p className='p2'>Note: These details will represent the fields in your resume.</p>
 
-            <form method='POST' onSubmit={handleFormSubmit} className='details1form'>
-                <div className='details-box'>
-                    <label className='details-lbl'>Fullname:</label> <br/>
-                    <input 
-                        className='border'
-                        type='text'
-                        maxLength={100}
-                        onChange={(e) => setFullname(e.target.value)}
-                        required
-                    />
+                    <p className='form-heading'>(GENERAL INFORMATION)</p>
+
+                    <form method='POST' onSubmit={handleFormSubmit} className='details1form'>
+                        <div className='details-box'>
+                            <label className='details-lbl'>Fullname:</label> <br/>
+                            <input 
+                                className='border'
+                                type='text'
+                                maxLength={100}
+                                onChange={(e) => setFullname(e.target.value)}
+                                required
+                            />
+                        </div>
+
+                        <div className='details-box'>
+                            <label>Address:</label><br/>
+                            <input 
+                                type='text'
+                                className='border'
+                                maxLength={250}
+                                onChange={(e) => setAddress(e.target.value)}
+                                required
+                            />
+                        </div>
+
+                        <div className='details-box'>
+                            <label>Phone:</label><br/>
+                            <input 
+                                type='phone'
+                                className='border'
+                                onChange={(e) => setPhone(e.target.value)}
+                                required
+                            />
+                        </div>                    
+
+                        <div className='details-box'>
+                            <label>Email:</label><br/>
+                            <input 
+                                type='email'
+                                className='border'
+                                maxLength={250}
+                                onChange={(e) => setEmail(e.target.value)}
+                                required
+                            />
+                        </div>
+
+                        <div className='details-box'>
+                            <label>Tell us something about yourself...</label><br/>
+                            <textarea 
+                                type='text'
+                                className='textarea border'
+                                onChange={(e) => setDetails(e.target.value)}
+                                required
+                            />
+                        </div>
+
+                        <button type='submit' className='next-btn'>
+                            Next
+                        </button>
+                    </form>
                 </div>
-
-                <div className='details-box'>
-                    <label>Address:</label><br/>
-                    <input 
-                        type='text'
-                        className='border'
-                        maxLength={250}
-                        onChange={(e) => setAddress(e.target.value)}
-                        required
-                    />
-                </div>
-
-                <div className='details-box'>
-                    <label>Phone:</label><br/>
-                    <input 
-                        type='phone'
-                        className='border'
-                        onChange={(e) => setPhone(e.target.value)}
-                        required
-                    />
-                </div>                    
-
-                <div className='details-box'>
-                    <label>Email:</label><br/>
-                    <input 
-                        type='email'
-                        className='border'
-                        maxLength={250}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                    />
-                </div>
-
-                <div className='details-box'>
-                    <label>Tell us something about yourself...</label><br/>
-                    <textarea 
-                        type='text'
-                        className='textarea border'
-                        onChange={(e) => setDetails(e.target.value)}
-                        required
-                    />
-                </div>
-
-                <button type='submit' className='next-btn'>
-                    Next
-                </button>
-            </form>
-        </div>
+        }     
+        {/* <div>
+            <Resume data={details1Array} />
+        </div> */}
     </div>
   )
 }
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,34 +1,11 @@
 import React, { useState, useEffect } from 'react';
 
-const Resume = () => {
-  const [resumeData1, setResumeData1] = useState([]);
-  const [resumeData2, setResumeData2] = useState([]);
-
-  useEffect(() => {
-    const storedData1 = localStorage.getItem('generalInformation');
-    const storedData2 = localStorage.getItem('personalData');
-
-    console.log('Stored Data 1:', storedData1);
-    console.log('Stored Data 2:', storedData2);
-
-    if (storedData1) {
-      setResumeData1(JSON.parse(storedData1));
-    }
-    if (storedData2) {
-      setResumeData2(JSON.parse(storedData2));
-    }
-  }, []);
-
-  console.log('Resume Data 1:', resumeData1);
-  console.log('Resume Data 2:', resumeData2);
+const Resume = (props) => {
+    // const {fullname, address, phone, email, details} = props.data
 
   return (
     <div>
-      <p>Stored data from General Information:</p>
-      <pre>{JSON.stringify(resumeData1, null, 2)}</pre>
-
-      <p>Stored data from Personal Data:</p>
-      <pre>{JSON.stringify(resumeData2, null, 2)}</pre>
+        <p>Resume</p>
     </div>
   );
 };
